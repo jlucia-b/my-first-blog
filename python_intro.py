c47ca9c38c36b92ac80df3b0ef54ec0c317331fd
jlucia-b <lucia.barreraoro@gmail.com>
2019-12-04 18:40:59 -0300

    Mi aplicación Django Girls, primer commit

---
+++ python_intro.py
@@ -0,0 +1,40 @@
+#def hi():
+#    print('Hi there!')
+#    print('How are you?')
+
+#hi()
+
+
+#def hi(name):
+#    if name == 'lucia':
+#        print('hi lucia')
+#    elif name == 'djangogirls':
+#        print ('hi djangogirls')
+#    else:
+#        print('hi anonymous')
+
+#hi('stitch')
+
+#def hi (name):
+#    print('hi ' + name + '!')
+#girls = ['Rachel', 'monica', 'phoebe', 'lucia']
+#for name in girls:
+#    hi (name)
+#    print('next girl')
+
+#for i in range(1,8):
+#    print(i)
+
+volume = 57
+if volume < 20:
+    print("It's kinda quiet.")
+elif 20 <= volume < 40:
+    print("It's nice for background music")
+elif 40 <= volume < 60:
+    print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+    print("Nice for parties")
+elif 80 <= volume < 100:
+    print("A bit loud!")
+else:
+    print("Me duelen las orejas! :(")
